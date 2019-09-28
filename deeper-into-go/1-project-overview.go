60b8713656016b249ea9658d68c1fba108c67e27
VnkHO <ho.vincent.nk@gmail.com>
2019-09-28 21:36:16 +0200

    Project - Deeper into Go - Cards

---
+++ deeper-into-go/1-project-overview.go
@@ -0,0 +1,12 @@
+/*
+
+	Create a package that's going to simulate playing around with a deck of playing cards.
+
+	newDeck 				-> Create a list of playing cards. Essentially an array of strings
+	print 					-> Log out the contents of a deck of cards
+	shuffle 				-> Shuffles all the cards in a deck
+	deal 						-> Create a 'hand' of cards
+	saveToFile 			-> Save a list of cards to a file on the local machine
+	newDeckFromFile -> Loead a list of cards from the local machine
+
+*/
