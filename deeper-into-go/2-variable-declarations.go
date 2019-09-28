f7b3f7419b01125caa628137f78ce51e00b73e04
VnkHO <ho.vincent.nk@gmail.com>
2019-09-28 21:49:46 +0200

    Variables declarations

---
+++ deeper-into-go/2-variable-declarations.go
@@ -0,0 +1,40 @@
+/*
+
+	var card string = "Ace of Spades"
+
+	var 		-> We are about to create a new variable
+	card 		-> The name of the variable will be 'greeting'
+	string	-> Only a 'string' will ever be assigned to this variable
+	"Ace of Spades" -> Assign the value "Ace of Spades" to this variable
+
+
+	Static Types languages:
+		- C++
+		- Java
+		- Go
+		- ...
+
+	Dynamic Types languages:
+		- JavaScript
+		- Ruby
+		- Python
+		- ...
+
+		Basic Go Types
+
+		Type:
+			- bool 				-> true false
+			- string 			-> "Hi !" // "How is it going ?"
+			- int					-> 0 	//	-1000	//	654634
+			- float64			-> 10.053252	//	0.32424	//	-123.534
+			...
+
+
+		var card string = "Ace of Spades"
+		card := "Ace of Spades" // Alternative way
+
+		:= 	//	We only use this colon equals syntax when we are defining a new variable
+		card = 'Five of Diamonds	// Works
+
+
+*/
