8ab877f90685b3e0babde0f67f76e6987d164f21
VnkHO <ho.vincent.nk@gmail.com>
2019-09-29 17:07:35 +0200

    Return multiple values in Go

---
+++ deeper-into-go/10-return-multiple-values.go
@@ -0,0 +1,14 @@
+/*
+
+	// Return 2 values
+	func deal(d deck, handSize int) (deck, deck) {
+
+	}
+
+	hand, remaininDeck = deal(cards, 5)
+
+																	'to remainingDeck'
+	hand, remainingDeck := <deck>, <deck>
+													'to hand'
+
+*/
