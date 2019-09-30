bd3b2bbcc568a7c4de8168db356ae90adb6ed3a0
VnkHO <ho.vincent.nk@gmail.com>
2019-09-30 22:01:54 +0200

    Testing with Go

---
+++ deeper-into-go/19-testing-with-go.go
@@ -0,0 +1,11 @@
+/*
+
+	Go testing is not RSpec, mocja, jasmine, selenium ,etc...package into
+
+	To make a test, create a new file ending in _test.go
+		deck_test.go
+
+	To run all tests in a package, run the command...
+		go test
+
+*/
