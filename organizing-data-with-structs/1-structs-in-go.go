8b3d1bcbee6511c393656fe01be5645d307b58a4
VnkHO <ho.vincent.nk@gmail.com>
2019-10-01 22:25:12 +0200

    Organizing Data with Struct - Struct in Go

---
+++ organizing-data-with-structs/1-structs-in-go.go
@@ -0,0 +1,20 @@
+/*
+
+	Structs
+		-> Data structure. Collection of properties that are related together
+
+	Example: card "Ace of Spades"
+
+		Card struct Field Definition
+
+		suit	-> <string>
+		value	-> <string>
+
+		Card Struct
+
+		suit	-> "Spades"
+		value	-> "Ace"
+
+		In JavaScript it's like a plain object (but not the same)
+
+*/
