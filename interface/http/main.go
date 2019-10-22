6e37f53401ded9178f12a2d818b12a87528df3c5
VnkHO <ho.vincent.nk@gmail.com>
2019-10-22 19:20:34 +0200

    Interface

---
+++ interface/http/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("https://www.google.fr")
+	if err != nil {
+		fmt.Println("Error :", err)
+		os.Exit(1)
+	}
+	fmt.Println(response)
+}
